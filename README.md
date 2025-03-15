bc43ec9c7f0a358f570e878211af8b3c7e855d56
huafengThunder <437832424@qq.com>
2025-03-15 22:33:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#### 收集/编辑一些Tampermonkey脚本
+#### 将浏览器的窗口调成对应站点的Tampermonkey（篡改猴）
+- Tampermonkey能够保持上文，所有跨窗口也是没有问题的
---
+++ del-qq-email/index.js
@@ -0,0 +1,47 @@
+
+function clickCompletelyDelete(targetEmail) {
+    const targetUElements = document.querySelectorAll('.cx')
+    const arr = Array.from(targetUElements);
+    arr.forEach(u => {
+        const innerText = u.nextSibling.nextElementSibling.querySelector('.tl.tf  span').getAttribute('n')
+        if (targetEmail.includes(innerText)) {
+            const checkbox = u.querySelector('input[type="checkbox"][name="mailid"]')
+            if (checkbox) {
+                checkbox.checked = true
+            }
+        }
+    })
+    document.querySelector('#quick_completelydel').click()
+}
+const targetEmail = [
+    '阿里云',
+    'Gateio',
+    'GitHub',
+    'Apple',
+    'The brightonSEO team',
+    'infogaming',
+    'TAPD',
+    'Eric at StackBlitz',
+    'Swydo & The brightonSEO team',
+    'Vue Mastery',
+    'OKX',
+    'IOTE上海物联网展',
+    'npm',
+    'IOTE物联网展',
+    'Apple Music',
+    'lbs(腾讯位置服务)',
+    'Binance',
+    '12306@rails.com.cn',
+    'Microsoft 帐户团队',
+    'CSDN',
+    'Gitee',
+    'Find My',
+    'Marius Gherasie',
+    'CodePen',
+    'Pelion',
+    'UCloud 云计算'
+]
+clickCompletelyDelete(targetEmail)
+setTimeout(() => {
+    top.document.querySelector('#QMconfirm_QMDialog_confirm').click()
+}, 600)
