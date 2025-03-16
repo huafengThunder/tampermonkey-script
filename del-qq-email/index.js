4bce6b908c0297bf12df95911861fcc5c8b60147
huafengThunder <437832424@qq.com>
2025-03-16 13:56:45 +0800

    方便操作

--- del-qq-email/index.js
+++ del-qq-email/index.js
@@ -1,5 +1,32 @@
-
-function clickCompletelyDelete(targetEmail) {
+function clickCompletelyDelete() {
+    const targetEmail = [
+        '阿里云',
+        'Gateio',
+        'GitHub',
+        'Apple',
+        'The brightonSEO team',
+        'infogaming',
+        'TAPD',
+        'Eric at StackBlitz',
+        'Swydo & The brightonSEO team',
+        'Vue Mastery',
+        'OKX',
+        'IOTE上海物联网展',
+        'npm',
+        'IOTE物联网展',
+        'Apple Music',
+        'lbs(腾讯位置服务)',
+        'Binance',
+        '12306@rails.com.cn',
+        'Microsoft 帐户团队',
+        'CSDN',
+        'Gitee',
+        'Find My',
+        'Marius Gherasie',
+        'CodePen',
+        'Pelion',
+        'UCloud 云计算'
+    ]
     const targetUElements = document.querySelectorAll('.cx')
     const arr = Array.from(targetUElements);
     arr.forEach(u => {
@@ -13,35 +40,7 @@ function clickCompletelyDelete(targetEmail) {
     })
     document.querySelector('#quick_completelydel').click()
 }
-const targetEmail = [
-    '阿里云',
-    'Gateio',
-    'GitHub',
-    'Apple',
-    'The brightonSEO team',
-    'infogaming',
-    'TAPD',
-    'Eric at StackBlitz',
-    'Swydo & The brightonSEO team',
-    'Vue Mastery',
-    'OKX',
-    'IOTE上海物联网展',
-    'npm',
-    'IOTE物联网展',
-    'Apple Music',
-    'lbs(腾讯位置服务)',
-    'Binance',
-    '12306@rails.com.cn',
-    'Microsoft 帐户团队',
-    'CSDN',
-    'Gitee',
-    'Find My',
-    'Marius Gherasie',
-    'CodePen',
-    'Pelion',
-    'UCloud 云计算'
-]
-clickCompletelyDelete(targetEmail)
+clickCompletelyDelete()
 setTimeout(() => {
     top.document.querySelector('#QMconfirm_QMDialog_confirm').click()
 }, 600)
